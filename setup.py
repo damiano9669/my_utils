cce129680330c56e311d034acee227d0edb175cc
damiano <damiano.derin@gmail.com>
2018-12-07 20:48:40 +0100

    add init and setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='my_utils',
+    version='1.0.0',
+    packages=['my_utils', 'my_utils.web', 'my_utils.web.telegram', 'my_utils.math', 'my_utils.prints',
+              'my_utils.dir_file', 'my_utils.data_structures'],
+    url='https://github.com/damiano9669/my_utils.git',
+    license='',
+    author='damiano derin',
+    author_email='damiano.derin@gmail.com',
+    description='simple utils'
+)
