7ca26bb7b51dc18f83f71377e949c36d07986cf3
damiano <damiano.derin@gmail.com>
2018-12-09 19:03:56 +0100

    setup.py modified

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='my_utils',
     version='1.0.0',
-    packages=['my_utils', 'my_utils.web', 'my_utils.web.telegram', 'my_utils.math', 'my_utils.prints',
-              'my_utils.dir_file', 'my_utils.data_structures'],
     url='https://github.com/damiano9669/my_utils.git',
+    packages=find_packages(),
     license='',
     author='damiano derin',
     author_email='damiano.derin@gmail.com',
