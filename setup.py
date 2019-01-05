1521372a0d38083ad95db04ad9bd22be86a0ba3a
damiano9669 <damiano.derin@gmail.com>
2019-01-05 16:20:44 +0100

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='my_utils',
-    version='2.0.0',
+    version='2.0.1',
     url='https://github.com/damiano9669/my_utils.git',
     packages=find_packages(),
     license='',
