38d9e3e36b4dd36025799d95ef48ea22ed5887c5
damiano9669 <damiano.derin@gmail.com>
2019-01-05 16:16:22 +0100

    update prints

--- my_utils/prints/prints.py
+++ my_utils/prints/prints.py
@@ -1,21 +1,23 @@
-from my_utils import prints as cl
+import my_utils.prints.colors as cl
 
 
 def color(color, name, value=''):
     print(color + str(name) + cl.ENDC, value)
 
+
 def red(name, value=''):
     color(cl.FAIL, name, value=value)
 
+
 def green(name, value=''):
     color(cl.OKGREEN, name, value=value)
+
 
 def blue(name, value=''):
     color(cl.OKBLUE, name, value=value)
 
 
 if __name__ == '__main__':
-
     red('ciao', 5)
     green('ciao', 5)
     blue('ciao', 5)
