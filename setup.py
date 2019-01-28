f1cafc2e28703131cf759899dd2119c5d634acf6
damiano9669 <damiano.derin@gmail.com>
2019-01-28 19:34:50 +0100

    new version with bluetooth manager

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='my_utils',
-    version='2.2.2',
+    version='3.0.0',
     url='https://github.com/damiano9669/my_utils.git',
     packages=find_packages(),
     license='',
