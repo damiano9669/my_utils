605712a02defc99c174c35496fdcb590d494ef74
damiano9669 <damiano.derin@gmail.com>
2019-02-03 11:28:26 +0100

    cryptography edited

--- my_utils/cryptography/asymmetric_crypto.py
+++ my_utils/cryptography/asymmetric_crypto.py
@@ -1,11 +1,17 @@
+import os
+
 from Crypto import Random
+from Crypto.Cipher import PKCS1_v1_5
 from Crypto.PublicKey import RSA
 import base64
+
+import my_utils.dir_file.files as fl
+import my_utils.dir_file.directories as d
 
 
 def generate_keys():
     """
-    :return: private and public keys
+    :return: private and public keys in RSA_object
     """
     # key length must be a multiple of 256 and >= 1024
     modulus_length = 256 * 4
@@ -17,30 +23,72 @@ def generate_keys():
 def encrypt(raw_message, public_key):
     """
         to encrypt message
-    :param raw_message: byte string -> message to encrypt
-    :param public_key: public key
-    :return: encoded in base64 and encrypted message
+    :param raw_message: string message to encrypt
+    :param public_key: RSA_object key
+    :return:
     """
-    encrypted_msg = public_key.encrypt(raw_message, 32)[0]
-    encoded_encrypted_msg = base64.b64encode(encrypted_msg)
-    return encoded_encrypted_msg
+    cipher = PKCS1_v1_5.new(public_key)
+    ciphertext = base64.b64encode(cipher.encrypt(raw_message))
+    return ciphertext
 
 
-def decrypt(encoded_encrypted_msg, private_key):
+def decrypt(encrypted_msg, private_key):
     """
         to decrypt message
-    :param encoded_encrypted_msg: message encrypted
-    :param private_key: private key
-    :return: decoded and decrypted message
+    :param encrypted_msg: byte array encrypted message
+    :param private_key: RSA_object key
+    :return:
     """
-    decoded_encrypted_msg = base64.b64decode(encoded_encrypted_msg)
-    decoded_decrypted_msg = private_key.decrypt(decoded_encrypted_msg)
-    return decoded_decrypted_msg
+    cipher = PKCS1_v1_5.new(private_key)
+    plaintext = cipher.decrypt(base64.b64decode(encrypted_msg), 'Error while decrypting')
+
+    return plaintext
+
+
+def save_key(key, path):
+    """
+        to save in txt the RSA_object key
+    :param key: RSA_object
+    :param path: file_path
+    :return:
+    """
+    key_string = key.exportKey()
+    fl.check_if_file_exists(path, create=True)
+    fl.write(path, key_string.decode('utf-8'))
+    return True
+
+
+def load_key(path):
+    """
+        to load the key from file txt
+    :param path: file_txt of the key
+    :return: RSA_object key
+    """
+    key_string = fl.read(path)
+    RSA_key_obj = RSA.importKey(key_string)
+    return RSA_key_obj
 
 
 if __name__ == '__main__':
+
+    keys_path = 'my_keys'
+    pub_file = 'pub.txt'
+    priv_file = 'priv.txt'
+
+    pub_path = os.path.join(keys_path, pub_file)
+    priv_path = os.path.join(keys_path, priv_file)
+
+    d.check_if_dir_exists(keys_path, create=True)
+    if fl.check_if_file_exists(pub_path):
+        fl.erase(pub_path)
+        fl.erase(priv_path)
+
     priv_key, pub_key = generate_keys()
-    print(priv_key, pub_key)
+    save_key(pub_key, pub_path)
+    save_key(priv_key, priv_path)
+
+    priv_key = load_key(priv_path)
+    pub_key = load_key(pub_path)
 
     message = b'Un messaggio da crittare.'
     print(message)
