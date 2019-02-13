e83953cb96df9d226665bb5ed6b526b394e3f530
damiano9669 <damiano.derin@gmail.com>
2019-02-13 14:03:35 +0100

    cryptography aes_cipher added

---
+++ my_utils/cryptography/aes_cipher.py
@@ -0,0 +1,43 @@
+import base64
+import hashlib
+from Crypto import Random
+from Crypto.Cipher import AES
+
+
+class aes_cipher:
+
+    def __init__(self, key):
+        """
+        :param key: byte key
+        """
+        self.bs = 16
+        self.key = hashlib.sha256(key.encode()).digest()
+
+    def encrypt(self, message):
+        """
+            to encrypt message
+        :param message: string to encrypt
+        :return:
+        """
+        message = self._pad(message)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(message)).decode('utf-8')
+
+    def decrypt(self, enc):
+        """
+            to decrypt message
+        :param enc: encrypted message
+        :return:
+        """
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s) - 1:])]
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='my_utils',
-    version='4.1.0',
+    version='4.2.0',
     url='https://github.com/damiano9669/my_utils.git',
     packages=find_packages(),
     license='',
